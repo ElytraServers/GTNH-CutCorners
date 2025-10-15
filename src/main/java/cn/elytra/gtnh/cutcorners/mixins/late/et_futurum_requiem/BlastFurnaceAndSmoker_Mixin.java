c8883831eb8c28f9892c654e10dab605b2bd656e
Taskeren <r0yalist@outlook.com>
2025-10-15 20:06:54 +0800

    Added Blast Furnace and Smoker inject

---
+++ src/main/java/cn/elytra/gtnh/cutcorners/mixins/late/et_futurum_requiem/BlastFurnaceAndSmoker_Mixin.java
@@ -0,0 +1,18 @@
+package cn.elytra.gtnh.cutcorners.mixins.late.et_futurum_requiem;
+
+import cn.elytra.gtnh.cutcorners.CutCorners;
+import ganymedes01.etfuturum.tileentities.TileEntityBlastFurnace;
+import ganymedes01.etfuturum.tileentities.TileEntitySmoker;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(value = { TileEntitySmoker.class, TileEntityBlastFurnace.class }, remap = false)
+public class BlastFurnaceAndSmoker_Mixin {
+
+    @ModifyConstant(method = "updateEntity", constant = @Constant(intValue = 100))
+    private int gtnhcc$getMaxSmeltingTime(int value) {
+        return Math.max(1, CutCorners.getStrategy().getMaxFurnaceSmeltingTime(value) / 2);
+    }
+
+}
