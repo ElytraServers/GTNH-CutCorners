59ff8233b8be96cc9b11c848cd6cc8d69d7af3f3
Taskeren <r0yalist@outlook.com>
2026-01-03 11:57:49 +0800

    Don't heat up solar boilers without sufficient water

--- src/main/java/cn/elytra/gtnh/cutcorners/mixins/late/gregtech/MTEBoilerMixin.java
+++ src/main/java/cn/elytra/gtnh/cutcorners/mixins/late/gregtech/MTEBoilerMixin.java
@@ -4,6 +4,7 @@ import cn.elytra.gtnh.cutcorners.CutCorners;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import gregtech.common.tileentities.boilers.MTEBoiler;
+import gregtech.common.tileentities.boilers.MTEBoilerSolar;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,6 +18,10 @@ public abstract class MTEBoilerMixin {
     @WrapOperation(method = "calculateHeatUp", at = @At(value = "INVOKE", target = "Lgregtech/common/tileentities/boilers/MTEBoiler;getHeatUpAmount()I"))
     private int gtnhcc$modifyHeatUp(MTEBoiler instance, Operation<Integer> original) {
         if (CutCorners.getStrategy().isImmediateMode()) {
+            if(instance instanceof MTEBoilerSolar && instance.mFluid.amount < 1000) {
+                // don't heat up solar boilers immediately if it doesn't have sufficient water.
+                return 0;
+            }
             return getMaxTemperature();
         }
         return original.call(instance);
