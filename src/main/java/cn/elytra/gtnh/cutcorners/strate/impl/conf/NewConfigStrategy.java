e365bb0f0c5c78838d8117917725f7078a449937
Taskeren <r0yalist@outlook.com>
2026-03-18 14:14:12 +0800

    Fix compilation error

--- src/main/java/cn/elytra/gtnh/cutcorners/strate/impl/conf/NewConfigStrategy.java
+++ src/main/java/cn/elytra/gtnh/cutcorners/strate/impl/conf/NewConfigStrategy.java
@@ -8,6 +8,7 @@ import cn.elytra.gtnh.cutcorners.mixins.late.railcraft.BlastFurnaceRecipeAccesso
 import cn.elytra.gtnh.cutcorners.mixins.late.railcraft.CokeOvenRecipeAccessor;
 import cn.elytra.gtnh.cutcorners.strate.ICutCornerStrategy;
 import cn.elytra.gtnh.cutcorners.util.ResearchStationHelper;
+import com.google.common.collect.Sets;
 import gregtech.api.enums.TierEU;
 import gregtech.api.recipe.RecipeMap;
 import gregtech.api.util.GTRecipe;
@@ -20,19 +21,18 @@ import tectech.recipe.EyeOfHarmonyRecipe;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 public class NewConfigStrategy implements ICutCornerStrategy {
 
-    private final Set<String> blacklistedRecipeMaps;
-
     @NotNull
     private final CutCornersConfig config;
+    @NotNull
+    private final Set<String> blacklistedRecipeMaps;
 
     public NewConfigStrategy(CutCornersConfig config) {
         this.config = Objects.requireNonNull(config);
-        this.blacklistedRecipeMaps = new HashSet<>(
-            Arrays.asList(config.getGregTechBlacklistedRecipeMaps())
-        );
+        this.blacklistedRecipeMaps = Sets.newHashSet(config.getGregTechBlacklistedRecipeMaps());
     }
 
     @Override
